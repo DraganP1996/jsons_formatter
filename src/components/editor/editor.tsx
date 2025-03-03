2fc189e54fd31d4ce5ebce66a01812e4006212e9
Dragan Petrovic <pdragan1996@gmail.com>
2025-03-03 22:45:05 +0100

    Added support for folding and unfolding all

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { CollapseAllIcon, ExpandAllIcon } from "@/icons";
 import { JsonEditor } from "webeditors-react";
 import { Button } from "../ui/button";
+import { useRef } from "react";
 
 type EditorProps = {
   value: string;
@@ -21,17 +24,30 @@ export const Editor = ({
   theme,
   readonly = false,
 }: EditorProps) => {
+  const editorRef = useRef<HTMLJsonEditorElement>(null);
   console.log({ value, tabSize, mode });
+
+  const handleFoldAll = () => {
+    if (!editorRef.current) return;
+
+    editorRef.current.foldAll();
+  };
+
+  const handleUnfoldAll = () => {
+    if (!editorRef.current) return;
+
+    editorRef.current.unfoldAll();
+  };
 
   return (
     isReady && (
       <div className="max-h-[1000px] overflow-hidden border-dashed border-blue-200 m-2 rounded-xl shadow-sm shadow-slate-400">
-        <JsonEditor theme={theme} value={value} readonly={readonly}>
+        <JsonEditor ref={editorRef} theme={theme} value={value} readonly={readonly}>
           <div slot="panel" className="flex flex-row p-1">
-            <Button variant="ghost" className="p-2">
+            <Button variant="ghost" className="p-2" onClick={handleFoldAll}>
               <CollapseAllIcon />
             </Button>
-            <Button variant="ghost" className="p-2">
+            <Button variant="ghost" className="p-2" onClick={handleUnfoldAll}>
               <ExpandAllIcon />
             </Button>
           </div>
