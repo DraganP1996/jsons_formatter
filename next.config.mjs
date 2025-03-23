b2ac3ed032cb349132e6f3d020125f6e4ddc5d82
Dragan Petrovic <pdragan1996@gmail.com>
2025-03-23 10:51:48 +0100

    Modify next config to fix deployment issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import createMDX from "@next/mdx";
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 import remarkGfm from "remark-gfm";
 
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+// const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const nextConfig = {
   transpilePackages: ["@webeditors/react", "webeditors-vanilla"],
-  outputFileTracingRoot: path.join(__dirname, "../"),
+  // outputFileTracingRoot: path.join(__dirname, "../"),
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
